b2dda3f804303cacbc38c59a05f5938ce8b8cf81
Michael Chen <michaelchen@squareup.com>
2019-03-06 23:59:35 -0800

    initial setup stuff

---
+++ db/migrate/20190307075134_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :date_of_birth
+      t.string :occupation
+      t.string :about
+      t.string :school
+      t.string :company
+
+      t.timestamps
+    end
+  end
+end
